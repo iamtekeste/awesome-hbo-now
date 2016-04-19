3c91e72f7998fa79df04764873c6fb0365ae5e12
iamtekeste <iamtekeste@gmail.com>
2016-04-18 21:47:58 -0500

    fix this ref

--- extension/bundle.js
+++ extension/bundle.js
@@ -6,10 +6,10 @@ var getMediaBoxes = function getMediaBoxes() {
 
 var getMediaBoxObj = function getMediaBoxObj(mediaBoxes) {
   var titles = [];
-  mediaBoxes.each(function (i) {
+  mediaBoxes.each(function (i, el) {
     var obj = {
       index: i,
-      title: $(undefined).find(".now-thumbnail-bottomtext").text().trim()
+      title: $(el).find(".now-thumbnail-bottomtext").text().trim()
     };
     titles.push(obj);
   });
@@ -24,29 +24,40 @@ function fetchIMDBData(movieObjs, cb) {
   var movies_data = new Object();
   var count = 0;
   movieObjs.forEach(function (movie) {
+
+    var url = "http://www.omdbapi.com/?plot=short&r=json&t=" + encodeURIComponent(movie.title);
     $.ajax({
-      url: "http://www.omdbapi.com/?plot=short&r=json&t=" + encodeURIComponent(movie.title),
+      url: url,
       type: "GET",
       success: function success(data) {
-        movies_data[count] = data;
+        data.index = count;
+        func(data);
+        // data.index = count;
+        // movies_data[count] = data;
         count++;
-        if (count === length) {
-          cb(movies_data);
-        }
+        // if (count === length) {
+        //   cb(movies_data);
+        // }
       }
     });
   });
 };
-
+function func(data) {
+  console.log(data);
+}
 $(document).ready(function () {
   //get the movie/series boxes
   var mediaBoxes = getMediaBoxes();
   //foreach movie box get the title
   var mediaBoxObjs = getMediaBoxObj(mediaBoxes);
-  var movies = fetchIMDBData(mediaBoxObjs, function (moviesData) {
-    console.log(moviesData);
+  fetchIMDBData(mediaBoxObjs, function (moviesData) {
+    //updateDom(mediaBoxes, moviesData);
   });
 });
+
+function updateDom(mediaBoxes, moviesData) {}
+// console.log(moviesData[0]);
+
 
 //create the ahn-info node
 //append ahn-info to now-thumbnail-overlay
--- extension/content.js
+++ extension/content.js
@@ -4,10 +4,10 @@ var getMediaBoxes = function () {
 
 var getMediaBoxObj =  mediaBoxes => {
 	var titles = [];
-	mediaBoxes.each((i) =>{
+	mediaBoxes.each((i, el) =>{
 		var obj = {
 			index: i,
-			title : $(this).find(".now-thumbnail-bottomtext").text().trim(),
+			title : $(el).find(".now-thumbnail-bottomtext").text().trim(),
 		}
 		titles.push(obj);
 	});
@@ -22,29 +22,40 @@ function fetchIMDBData(movieObjs, cb){
   var movies_data = new Object();
   var count = 0;
   movieObjs.forEach(function(movie){
+
+    var url = "http://www.omdbapi.com/?plot=short&r=json&t=" + encodeURIComponent(movie.title);
     $.ajax({
-      url: "http://www.omdbapi.com/?plot=short&r=json&t=" + encodeURIComponent(movie.title),
+      url: url,
       type: "GET",
       success: function(data) {
-        movies_data[count] = data;
+        data.index = count;
+        func(data);
+        // data.index = count;
+        // movies_data[count] = data;
         count++;
-        if (count === length) {
-          cb(movies_data);
-        }
+        // if (count === length) {
+        //   cb(movies_data);
+        // }
       }
     });
   });
 };
-
+function func(data) {
+  console.log(data)
+}
 $(document).ready(function(){
-	//get the movie/series boxes
-var mediaBoxes = getMediaBoxes();
-//foreach movie box get the title
-var mediaBoxObjs = getMediaBoxObj(mediaBoxes);
-  var movies = fetchIMDBData(mediaBoxObjs, function(moviesData) {
-    console.log(moviesData)
-  });
+    	//get the movie/series boxes
+    var mediaBoxes = getMediaBoxes();
+    //foreach movie box get the title
+    var mediaBoxObjs = getMediaBoxObj(mediaBoxes);
+    fetchIMDBData(mediaBoxObjs, function (moviesData) {
+        //updateDom(mediaBoxes, moviesData);
+    });
 });
+
+function updateDom(mediaBoxes, moviesData) {
+  // console.log(moviesData[0]);
+}
 
 //create the ahn-info node
 //append ahn-info to now-thumbnail-overlay
